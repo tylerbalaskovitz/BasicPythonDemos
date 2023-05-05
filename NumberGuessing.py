126efd7d3bc684f7bc6916c54d5194ee682f2c75
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-05-05 01:17:07 -0400

    Adding a python number guessing game that is a little more sophisticated, using random numbers, imports, multiple blocks, and condition statements, and while statements

--- NumberGuessing.py
+++ NumberGuessing.py
@@ -1,3 +1,20 @@
 import random
 
-print(random.randrange(0, 11))
+#this by default will take a string even if it is a number, so it needs to be parsed into an int via the int method.
+maximumNumberSize = input("Type a number: ")
+
+#checks to make sure that something is a digit by using the isDigit() method
+if maximumNumberSize.isdigit():
+    maximumNumberSize = int(maximumNumberSize)
+    if maximumNumberSize <= 0:
+        print("Please type a number greater than 0.")
+        quit()
+else:
+    print("Please type a number next time")
+    quit()
+
+randomNumber = random.randint(0, maximumNumberSize)
+print(randomNumber)
+
+while True:
+    print("hello")
