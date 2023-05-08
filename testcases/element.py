591727da0bdff5794a891affa7719842c307be54
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-05-08 00:31:42 -0400

    sometimes you have to have the method creation on the furthest left tab/indent of the file in order for errors to not be thrown

--- testcases/element.py
+++ testcases/element.py
@@ -3,15 +3,15 @@
 from selenium.webdriver.support.ui import WebDriverWait
 
 #this is used for setting the value of an object. Essentially, this class is used for creating getters and setters within a class. 
-    def __set__(self, obj, value):
+def __set__(self, obj, value):
         driver = obj.driver
         WebDriverWait(driver, 100).until(
-            lambda driver: driver.find_element("name", self.locator))
+            lambda driver: driver.find_element_by_name(self.locator))
         driver.find_element_by_name(self.locator).clear()
         driver.find_element_by_name(self.locator).send_keys(value)
 
     #no longer have to wait for that element, but use the BasePageElement class instead
-    def __get__(self, obj, owner):
+def __get__(self, obj, owner):
         driver = obj.driver
         WebDriverWait(driver, 100).until(
             lambda driver: driver.find_element_by_name(self.locator))
