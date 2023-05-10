f12884ca50f314bd5954a99cbff8243ebe66b85b
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-05-09 20:07:37 -0400

    Solved an error by passing in the self.driver into the parenthesis of the page.MainPage() function

--- testcases/main.py
+++ testcases/main.py
@@ -6,7 +6,7 @@ class PythonOrgSearch(unittest.TestCase):
 
     #used at the beginning to begin running test cases.
     def setUp(self):
-        print("setup test")
+        print("--setup test--")
         self.driver = webdriver.Chrome("/home/tyler/Documents/WebDriver/chromedriver")
         self.driver.get("http://www.python.org")
 
@@ -16,8 +16,17 @@ class PythonOrgSearch(unittest.TestCase):
         assert False
 
     def test_title(self):
-        mainPage = page.MainPage()
+        mainPage = page.MainPage(self.driver)
         assert mainPage.is_title_matches()
+
+    def test_search_python(self):
+        mainPage = page.MainPage(self.driver)
+        #Comma says do the following operations afterwards
+        assert mainPage.is_title_matches()
+        mainPage.search_text_element = "pycon"
+        mainPage.click_go_button()
+        search_result_page = page.SearchResultPage(self.driver)
+        assert search_result_page.is_results_found()
 
     #Since this doesn't start with test_* it will not be executed with the class being tested
     def not_a_test(self):
